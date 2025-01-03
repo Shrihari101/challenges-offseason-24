41d48cb43881656b89fea44418ccd7419795ae8e
Shrihari Kalagi <shriharikalagi@gmail.com>
2025-01-03 13:00:16 -0500

    oh my sigma

--- src/main/java/frc/robot/subsystems/wrist/Wrist.java
+++ src/main/java/frc/robot/subsystems/wrist/Wrist.java
@@ -3,36 +3,46 @@ package frc.robot.subsystems.wrist;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Wrist extends SubsystemBase {
   // For instructions on how to implement this class, refer to the README.md file
+  WristIO m_io;
+  PIDController m_controller;
+  WristInputsAutoLogged m_inputs;
+  Rotation2d m_desiredAngle;
+  static final double TOLERANCE_DEGREES = 3.0;
 
   public Wrist(WristIO io, PIDController controller) {
-    // TODO: Implement the constructor
+    this.m_io = io;
+    this.m_controller = controller;
+    this.m_inputs = new WristInputsAutoLogged();
+    this.m_desiredAngle = Rotation2d.fromDegrees(0);
   }
 
   @Override
   public void periodic() {
-    // TODO: Implement this method
+    m_io.updateInputs(m_inputs);
+    double currentAngle = m_io.getAngle().getDegrees();
+    double pidOutput = m_controller.calculate(currentAngle, m_desiredAngle.getDegrees());
+    m_io.setVoltage(pidOutput);
   }
 
   public void setDesiredAngle(Rotation2d angle) {
-    // TODO: Implement this method
+    this.m_desiredAngle = angle;
   }
 
   public Command setDesiredAngleCommand(Rotation2d angle) {
-    // TODO: Implement this method
-    return null;
+    return new InstantCommand(() -> setDesiredAngle(angle));
   }
 
   public boolean withinTolerance() {
-    // TODO: Implement this method
-    return false;
+    return Math.abs(m_io.getAngle().getDegrees() - m_desiredAngle.getDegrees())
+        <= TOLERANCE_DEGREES;
   }
 
   public WristInputsAutoLogged getInputs() {
-    // TODO: Implement this method
-    return null;
+    return m_inputs;
   }
 }
